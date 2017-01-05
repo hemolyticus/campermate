dac6047d8ab973f8af82d9926c1ef7fe46480e18
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-01-06 12:32:50 +1300

    Camp Details page setup

--- src/pages/camp-details/camp-details.ts
+++ src/pages/camp-details/camp-details.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Data } from '../../providers/data';
 
 /*
   Generated class for the CampDetails page.
@@ -13,10 +15,12 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CampDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public dataService: Data) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CampDetailsPage');
+  saveForm(): void
+  {
+
+
   }
 
 }
